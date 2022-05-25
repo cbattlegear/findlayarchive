d17924d16d88c101d850f13676d0573da2fb34b3
Cameron Battagler <cameron@battagler.me>
2022-05-25 10:54:10 -0500

    Added AKA's and Basic jump to nav

--- src/Designs.vue
+++ src/Designs.vue
@@ -31,19 +31,8 @@ export default {
       ).json();
       window.document.title = `Designs | Findlay Archive`
     },
-    formatTags() {
-      var tagHtml = '<span class="fw-bold">Current Tags:</span> ';
-      this.tags.forEach((element) => {
-        tagHtml += '<span class="badge bg-secondary">' + element + "</span> \n";
-      });
-      return tagHtml;
-    },
-    truncate(v) {
-      const newline = v.indexOf("\n");
-      return newline > 0 ? v.slice(0, newline) : v;
-    },
-    formatDate(v) {
-      return v.replace(/T|Z/g, " ");
+    format_section_id(category) {
+      return category;
     },
   },
 };
@@ -64,24 +53,33 @@ export default {
     </div>
 
   </nav>
-  <div class="container">
-    <main v-if="design_list" class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+  <div v-if="design_list" class="container">
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <nav class="nav">
+        <span class="p-2">Jump to:</span><a v-for="{ design_category } in design_list.design_groups" :href="'#' + format_section_id(design_category)" class="nav-link" >{{design_category}}</a>
+      </nav>
       <div class="row mt-2" v-for="{ design_category, designs } in design_list.design_groups" :key="design_category">
-        <h2>{{ design_category }}</h2>
-        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
-          <div class="col p-2" v-for="{ design_name, design_image } in designs" :key="design_name">
+        <section :id="format_section_id(design_category)">
+          <h2>{{ design_category }}</h2>
+          <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+            <div class="col p-2" v-for="{ design_name, design_image, aka_names } in designs" :key="design_name">
 
-            <div class="card card-mid-width">
-              <v-lazy-image src-placeholder="/hat_holder.png" :src="IMAGE_ROOT + design_image" :alt="design_name" />
-              <div class="card-img-overlay">
-                <h5 class="card-title"
-                  style="text-shadow: -1px 1px 0 #FFF, 1px 1px 0 #FFF, 1px -1px 0 #FFF, -1px -1px 0 #FFF;">{{
-                      design_name
-                  }}</h5>
+              <div class="card card-mid-width">
+                <v-lazy-image src-placeholder="/hat_holder.png" :src="IMAGE_ROOT + design_image" :alt="design_name" />
+                <div class="card-img-overlay">
+                  <h5 class="card-title"
+                    style="text-shadow: -1px 1px 0 #FFF, 1px 1px 0 #FFF, 1px -1px 0 #FFF, -1px -1px 0 #FFF;">{{
+                        design_name
+                    }}</h5>
+                </div>
+                <div v-if="aka_names" class="card-body">
+                  <h6>Also known as</h6>
+                  <p class="card-text">{{ aka_names }}</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </section>
       </div>
     </main>
   </div>
